a354a45af242d79050bd1141ba4a6a4d9588d54e
authenticdreamer <tim.weisbarth@student.uni-tuebingen.de>
2023-06-26 20:55:33 +0200

    .

--- src/visualizations.py
+++ src/visualizations.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 from tabulate import tabulate
 
-def plot_models(models):
+def plot_models(name_of_farm, models):
   fig, ax = plt.subplots(3,1, figsize=(10,10))
 
   for i, model in enumerate(models):
@@ -13,12 +13,13 @@ def plot_models(models):
       ax[i].legend()  # Display the legend
 
   plt.tight_layout()  # To prevent overlapping of subplots
+  fig.suptitle(f"{name_of_farm} wind farm: Comparison of power predictions", fontsize=13, y=1.02) 
   plt.show()
 
-def plot_metrics(models):
+def plot_metrics(name_of_farm, models):
  
   # Extract the relevant details for the table
-  table_data = [[model["name"], model["rmse"], model["mae"]] for model in models]
+  table_data = [[name_of_farm + " " + model["name"], model["rmse"], model["mae"]] for model in models]
 
   # Define the headers
   headers = ['Model Name', 'RMSE', 'MAE']
